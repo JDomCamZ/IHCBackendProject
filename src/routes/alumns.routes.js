acf8fde5991894f5780340dfa81f855430271f0c
JDomCamZ <juansehermanito@gmail.com>
2022-12-18 01:54:10 -0500

    Return possible courses.

--- src/routes/alumns.routes.js
+++ src/routes/alumns.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Student = require('../models/student');
+const Course = require('../models/courses');
 
 router.get('/', async (req, res) => {
     
@@ -13,7 +14,15 @@ router.get('/', async (req, res) => {
 
 router.get('/:code/turn', async (req, res) => {
     
-    const course = await Student.findOne({code: req.params.code});
-    res.json(course.turn);
+    const student = await Student.findOne({code: req.params.code});
+    res.json(student.turn);
 
 });
+
+router.get('/:code/courses', async (req, res) => {
+
+    const student = await Student.findOne({code: req.params.code});
+    const courses = await Course.find({semester: student.semester});
+    console.log(courses);
+    res.json(courses);
+})
